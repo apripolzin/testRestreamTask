869e3f1c3fa909278bffb3d30fd04e194ffb7dbe
Alexey Pripolzin <apripolzin@mail.ru>
2018-07-27 12:49:02 +0300

    Project fish

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -1,11 +1,19 @@
 #include "mainwidget.h"
+#include <QDebug>
 
-MainWidget::MainWidget(QWidget *parent)
+MainWidget::MainWidget(QWidget *parent/* = nullptr*/)
     : QWidget(parent)
 {
+    this->setFixedSize(1280, 720);
+
 }
 
 MainWidget::~MainWidget()
 {
+}
 
+void MainWidget::paintEvent(QPaintEvent *e)
+{
+
+    QWidget::paintEvent(e);
 }
--- mainwidget.h
+++ mainwidget.h
@@ -2,14 +2,25 @@
 #define MAINWIDGET_H
 
 #include <QWidget>
+#include <QList>
+#include <QPixmap>
+#include <QPointer>
+
+#define IMG_FILES_PATH "images"
 
 class MainWidget : public QWidget
 {
     Q_OBJECT
 
 public:
-    MainWidget(QWidget *parent = 0);
+    MainWidget(QWidget *parent = nullptr);
     ~MainWidget();
+
+protected:
+    virtual void paintEvent(QPaintEvent *e) override;
+
+private:
+    QList<QPointer<QPixmap>> pixmapList;
 };
 
 #endif // MAINWIDGET_H
